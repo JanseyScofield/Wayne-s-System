6d031dd67ddd511e887ca83acea6bcf6dda4cc3e
JanseyScofield <janscofield10@gmail.com>
2025-01-03 23:40:09 -0300

    Incrementando a tabela de Usuario e criando a tabela de Funcao

--- app/models.py
+++ app/models.py
@@ -6,3 +6,12 @@ class Usuario(db.Model):
     nome_usuario = db.Column(db.String, nullable = False)
     senha = db.Column(db.String, nullable = False)
     data_criacao = db.Column(db.DateTime, default = datetime.now())
+    id_funcao = db.Column(db.Integer, nullable = False)
+    permisao_mansao = db.Column(db.Integer, nullable = False)
+    permisao_indutria = db.Column(db.Integer, nullable = False)
+    permisao_batcaverna = db.Column(db.Integer, nullable = False)
+
+
+class Funcao(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    descricao = db.Column(db.String, nullable = False)
