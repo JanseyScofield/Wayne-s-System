131eda971de220b6d3f54fd7b488c4bc7f85cd4a
JanseyScofield <janscofield10@gmail.com>
2025-01-04 00:39:52 -0300

    Corrigindo pequenos erros na model de Usuario

--- app/models.py
+++ app/models.py
@@ -2,13 +2,13 @@ from app import db
 from datetime import datetime
 
 class Usuario(db.Model):
-    id = db.Column(db.Integer, primary_key = True, auto_incremente = True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     nome_usuario = db.Column(db.String, nullable = False)
     senha = db.Column(db.String, nullable = False)
     data_criacao = db.Column(db.DateTime, default = datetime.now())
     id_funcao = db.Column(db.Integer, nullable = False)
     permisao_mansao = db.Column(db.Integer, nullable = False)
-    permisao_indutria = db.Column(db.Integer, nullable = False)
+    permisao_industria = db.Column(db.Integer, nullable = False)
     permisao_batcaverna = db.Column(db.Integer, nullable = False)
 
 
