7f870f737fdef2dd5c62cdda14e4fe18e28b30ad
JanseyScofield <janscofield10@gmail.com>
2025-01-03 22:55:38 -0300

    Começando  a configurar o banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app=app)
+migrate = Migrate(app=app, db=db)
 
 from app.views import tela_login
