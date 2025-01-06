a18cf46a89e99dbf8df1fcf19c014c13be5ddbd6
JanseyScofield <janscofield10@gmail.com>
2025-01-05 22:41:33 -0300

    Adicionando o campo 'nome' na tabela de usuarios

--- app/models.py
+++ app/models.py
@@ -3,14 +3,14 @@ from datetime import datetime
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    nome_usuario = db.Column(db.String, nullable = False)
-    senha = db.Column(db.String, nullable = False)
+    nome = db.Column(db.String(255), nullable = False)
+    nome_usuario = db.Column(db.String(50), nullable = False)
+    senha = db.Column(db.String(30), nullable = False)
     data_criacao = db.Column(db.DateTime, default = datetime.now())
     id_funcao = db.Column(db.Integer, nullable = False)
     permisao_mansao = db.Column(db.Integer, nullable = False)
     permisao_industria = db.Column(db.Integer, nullable = False)
     permisao_batcaverna = db.Column(db.Integer, nullable = False)
-
 
 class Funcao(db.Model):
     id = db.Column(db.Integer, primary_key = True)
