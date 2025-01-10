0241570b9764d14047e3f312c862bd2ea72b0209
JanseyScofield <janscofield10@gmail.com>
2025-01-10 09:20:12 -0300

    Adicionando tabela de itens e categorias dos itens

--- app/models.py
+++ app/models.py
@@ -8,13 +8,21 @@ class Usuario(db.Model):
     senha = db.Column(db.String(30), nullable = False)
     data_criacao = db.Column(db.DateTime, default = datetime.now())
     id_funcao = db.Column(db.Integer, nullable = False)
-    permisao_mansao = db.Column(db.Integer, nullable = False)
-    permisao_industria = db.Column(db.Integer, nullable = False)
-    permisao_batcaverna = db.Column(db.Integer, nullable = False)
     permisao_registrar = db.Column(db.Integer, nullable = False)
     permisao_editar = db.Column(db.Integer, nullable = False)
     permisao_deletar = db.Column(db.Integer, nullable = False)
 
 class Funcao(db.Model):
     id = db.Column(db.Integer, primary_key = True)
+    descricao = db.Column(db.String, nullable = False)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    nome = db.Column(db.String(255), nullable = False)
+    id_categoria = db.Column(db.Integer, nullable = False)
+    descricao = db.Column(db.String, nullable = True)
+    quantidade = db.Column(db.Integer, nullable = False)
+
+class CategoriasItens(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     descricao = db.Column(db.String, nullable = False)
