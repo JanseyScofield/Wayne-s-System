383ccf1843442101c4301838805fbe227c2cf9c5
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2025-01-11 16:18:51 +0000

    Criando rotas básicas para cada função no módulo de funcionários para adicionar o acesso a eles na  página principal

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from app import db
 from app.models import Usuario
 from flask import render_template, request, redirect, url_for, session
 
-@app.route('/login', methods = ["GET", "POST"])
+@app.route('/login', methods = ['GET', 'POST'])
 def tela_login():
     erro = None
     session.clear()
@@ -46,7 +46,37 @@ def funcionarios():
         'permisao_deletar' : session['usuario_logado']['deletar']
     }
 
-    return render_template('modulo_funcionarios.html', context=context)
+    return render_template('funcionarios/modulo_funcionarios.html', context=context)
+
+@app.route('/funcionarios/registrar', methods = ['GET', 'POST'])
+def registrar_funcionario():
+    if not session:
+        return redirect(url_for('tela_login'))
+    return render_template('funcionarios/registrar_funcionario.html')
+
+@app.route('/funcionarios/visualizar', methods = ['GET'])
+def visualizar_funcionario():
+    if not session:
+        return redirect(url_for('tela_login'))
+    return render_template('funcionarios/visualizar_funcionarios.html')
+
+@app.route('/funcionarios/visualizar/unico', methods = ['GET'])
+def visualizar_unico_funcionario():
+    if not session:
+        return redirect(url_for('tela_login'))
+    return render_template('funcionarios/visualizar_unico_funcionario.html')
+
+@app.route('/funcionarios/remover', methods = ['GET'])
+def remover_funcionario():
+    if not session:
+        return redirect(url_for('tela_login'))
+    return render_template('funcionarios/apagar_funcionario.html')
+
+@app.route('/funcionarios/editar', methods = ['GET'])
+def editar_funcionario():
+    if not session:
+        return redirect(url_for('tela_login'))
+    return render_template('funcionarios/editar_funcionario.html')
 
 @app.route('/inventario', methods = ['GET'])
 def inventario():
@@ -59,4 +89,4 @@ def inventario():
         'permisao_deletar' : session['usuario_logado']['deletar']
     }
 
-    return render_template('modulo_inventario.html', context=context)
+    return render_template('iventario/modulo_inventario.html', context=context)
